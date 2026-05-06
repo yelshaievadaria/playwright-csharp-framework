ec559973481f11b7cbaec7993d941896d7d02168
“yelshaievadaria” <yelshaievadaria@gmail.com>
2026-05-07 00:02:28 +0100

    Negative Login Tests

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -1,6 +1,31 @@
+using Microsoft.Playwright;
+
 namespace PlaywrightTests.Pages;
 
 public class LoginPage
 {
-    public string Url => "https://example.com/login";
+    private readonly IPage _page;
+
+    public LoginPage(IPage page)
+    {
+        _page = page;
+    }
+
+    // locators
+    private ILocator Username => _page.Locator("#user-name");
+    private ILocator Password => _page.Locator("#password");
+    private ILocator LoginButton => _page.Locator("#login-button");
+
+    // actions
+    public async Task GoToAsync()
+    {
+        await _page.GotoAsync("https://www.saucedemo.com");
+    }
+
+    public async Task LoginAsync(string username, string password)
+    {
+        await Username.FillAsync(username);
+        await Password.FillAsync(password);
+        await LoginButton.ClickAsync();
+    }
 }
--- Tests/LoginTests.cs
+++ Tests/LoginTests.cs
@@ -1,17 +1,60 @@
 ﻿using NUnit.Framework;
 using PlaywrightTests.Base;
+using PlaywrightTests.Pages;
 
 namespace PlaywrightTests.Tests;
 
 public class LoginTests : BaseTest
 {
     [Test]
-    public async Task OpenSauceDemo()
+    public async Task ValidLogin()
     {
-        await Page.GotoAsync("https://www.saucedemo.com");
+        var loginPage = new LoginPage(Page);
 
-        await Page.WaitForTimeoutAsync(5000);
+        await loginPage.GoToAsync();
+        await loginPage.LoginAsync("standard_user", "secret_sauce");
 
-        Assert.That(await Page.TitleAsync(), Does.Contain("Swag"));
+        await Page.WaitForTimeoutAsync(2000);
+
+        Assert.That(Page.Url, Does.Contain("inventory"));
+    }
+
+    [Test]
+    public async Task InvalidPassword()
+    {
+        var loginPage = new LoginPage(Page);
+
+        await loginPage.GoToAsync();
+        await loginPage.LoginAsync("standard_user", "wrong_password");
+
+        var error = Page.Locator("[data-test='error']");
+
+        Assert.That(await error.IsVisibleAsync(), Is.True);
+    }
+
+    [Test]
+    public async Task EmptyUsername()
+    {
+        var loginPage = new LoginPage(Page);
+
+        await loginPage.GoToAsync();
+        await loginPage.LoginAsync("", "secret_sauce");
+
+        var error = Page.Locator("[data-test='error']");
+
+        Assert.That(await error.IsVisibleAsync(), Is.True);
+    }
+
+    [Test]
+    public async Task LockedUser()
+    {
+        var loginPage = new LoginPage(Page);
+
+        await loginPage.GoToAsync();
+        await loginPage.LoginAsync("locked_out_user", "secret_sauce");
+
+        var error = Page.Locator("[data-test='error']");
+
+        Assert.That(await error.InnerTextAsync(), Does.Contain("locked out"));
     }
 }
